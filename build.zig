9000a347e54fad00fdf273f8c1cb6663791e90da
scemino <scemino74@gmail.com>
2025-07-17 21:06:32 +0200

    Update build for zig 0.14

--- build.zig
+++ build.zig
@@ -3,14 +3,95 @@ const sokol = @import("sokol");
 const sdl = @import("sdl");
 
 const Child = std.process.Child;
-var mod_zi: *std.Build.Module = undefined;
 
-fn sdkPath(comptime suffix: []const u8) []const u8 {
-    if (suffix[0] != '/') @compileError("relToPath requires an absolute path!");
-    return comptime blk: {
-        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
-        break :blk root_dir ++ suffix;
-    };
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const opt_platform = b.option(PlatformAndRenderer, "platform", "Platform to use: sdl, sdl_soft or sokol");
+    const platform_renderer = if (target.result.cpu.arch.isWasm()) .sokol else opt_platform orelse .sdl;
+
+    // create the platform module from the chosen platform + renderer
+    const mod_platform = getPlatformModule(b, .{
+        .platform_renderer = platform_renderer,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // build lib
+    const mod_zi = b.addModule("zimpact", .{
+        .root_source_file = b.path("src/zimpact/zimpact.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "platform", .module = mod_platform },
+        },
+    });
+    const lib = b.addStaticLibrary(.{
+        .name = "zimpact",
+        .root_module = mod_zi,
+    });
+    b.installArtifact(lib);
+
+    // build docs
+    const docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    var docs_step = b.step("docs", "Build docs");
+    docs_step.dependOn(&docs.step);
+    b.getInstallStep().dependOn(docs_step);
+
+    // build image converter png => qoi
+    const qoiconv_step = b.step("qoiconv", "Build qoiconv");
+    const qoiconv_exe = addQoiConverter(b);
+    qoiconv_step.dependOn(&qoiconv_exe.step);
+    b.installArtifact(qoiconv_exe);
+
+    // build image converter wav => qoa
+    const qoaconv_step = b.step("qoaconv", "Build qoaconv");
+    const qoaconv_exe = addQoaConverter(b);
+    qoaconv_step.dependOn(&qoaconv_exe.step);
+    b.installArtifact(qoaconv_exe);
+
+    const asset_dir = "samples/zdrop/assets";
+    const assets_step = b.step("zdrop_assets", "Build assets");
+    try buildAssets(b, .{
+        .assets_step = assets_step,
+        .asset_dir = asset_dir,
+        .qoiconv_exe = qoiconv_exe,
+        .qoaconv_exe = qoaconv_exe,
+    });
+    assets_step.dependOn(qoiconv_step);
+    assets_step.dependOn(qoaconv_step);
+
+    // build Z Drop sample
+    const sample: []const u8 = "zdrop";
+    // main module with sokol and cimgui imports
+    const mod_sample = b.createModule(.{
+        .root_source_file = b.path(b.fmt("samples/{s}/main.zig", .{sample})),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "zimpact", .module = mod_zi },
+        },
+    });
+
+    const dep_sokol = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    if (target.result.cpu.arch.isWasm()) {
+        try buildWasm(b, .{ .mod_main = mod_sample, .dep_sokol = dep_sokol, .assets_step = assets_step, .shell_file_path = b.path("web/shell.html") });
+    } else {
+        try buildNative(b, .{
+            .name = sample,
+            .mod_main = mod_sample,
+            .assets_step = assets_step,
+            .platform_renderer = platform_renderer,
+        });
+    }
 }
 
 pub const PlatformAndRenderer = enum {
@@ -31,146 +112,87 @@ fn getPlatformModule(b: *std.Build, options: PlatformCreateOptions) *std.Build.M
 
     // create common module
     const mod_common = b.createModule(.{
-        .root_source_file = .{ .cwd_relative = sdkPath("/src/zimpact/common.zig") },
+        .root_source_file = b.path("src/zimpact/common.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    var mod_platform: *std.Build.Module = undefined;
-    switch (options.platform_renderer) {
+    return switch (options.platform_renderer) {
         .sdl_soft => {
             const sdl_sdk = sdl.init(b, .{});
-            // SDL platform module
-            mod_platform = b.createModule(.{
-                .root_source_file = .{ .cwd_relative = sdkPath("/src/zimpact/platform_sdl_soft.zig") },
+            // SDL soft platform module
+            return b.createModule(.{
+                .root_source_file = b.path("src/zimpact/platform_sdl_soft.zig"),
                 .target = target,
                 .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "common", .module = mod_common },
+                    .{ .name = "sdl", .module = sdl_sdk.getNativeModule() },
+                },
             });
-            mod_platform.addImport("sdl", sdl_sdk.getNativeModule());
         },
         .sdl => {
             const sdl_sdk = sdl.init(b, .{});
             // SDL platform module
-            mod_platform = b.createModule(.{
-                .root_source_file = .{ .cwd_relative = sdkPath("/src/zimpact/platform_sdl.zig") },
+            return b.createModule(.{
+                .root_source_file = b.path("src/zimpact/platform_sdl.zig"),
                 .target = target,
                 .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "common", .module = mod_common },
+                    .{ .name = "sdl", .module = sdl_sdk.getNativeModule() },
+                },
             });
-            mod_platform.addImport("sdl", sdl_sdk.getNativeModule());
         },
         .sokol => {
-            // sokol platform module
-            mod_platform = b.createModule(.{
-                .root_source_file = .{ .cwd_relative = sdkPath("/src/zimpact/platform_sokol.zig") },
-                .target = target,
-                .optimize = optimize,
-            });
             const dep_sokol = b.dependency("sokol", .{
                 .target = target,
                 .optimize = optimize,
             });
-            mod_platform.addImport("sokol", dep_sokol.module("sokol"));
+            // sokol platform module
+            return b.createModule(.{
+                .root_source_file = b.path("src/zimpact/platform_sokol.zig"),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "common", .module = mod_common },
+                    .{ .name = "sokol", .module = dep_sokol.module("sokol") },
+                },
+            });
         },
-    }
-    mod_platform.addImport("common", mod_common);
-    return mod_platform;
+    };
 }
 
-pub fn getZimpactModule(b: *std.Build, options: PlatformCreateOptions) *std.Build.Module {
-    var mod_platform: *std.Build.Module = undefined;
-    mod_platform = getPlatformModule(b, .{
-        .platform_renderer = options.platform_renderer,
-        .target = options.target,
-        .optimize = options.optimize,
+const BuildNativeOptions = struct {
+    name: []const u8,
+    mod_main: *std.Build.Module,
+    assets_step: *std.Build.Step,
+    platform_renderer: PlatformAndRenderer,
+};
+
+fn buildNative(b: *std.Build, options: BuildNativeOptions) !void {
+    // for native platforms, build into a regular executable
+    const exe = b.addExecutable(.{
+        .name = options.name,
+        .root_module = options.mod_main,
     });
 
-    // zimpact module
-    mod_zi = b.addModule("zimpact", .{
-        .root_source_file = .{ .cwd_relative = sdkPath("/src/zimpact/zimpact.zig") },
-        .target = options.target,
-        .optimize = options.optimize,
-    });
-    mod_zi.addImport("platform", mod_platform);
-    return mod_zi;
-}
-
-pub fn build(b: *std.Build) !void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-    const platform = b.option([]const u8, "platform", "Plaftorm to use: sdl, sdl_soft or sokol") orelse "sdl";
-    var platform_renderer: PlatformAndRenderer = .sdl;
-    if (target.result.cpu.arch.isWasm()) {
-        platform_renderer = .sdl;
-    } else if (std.mem.eql(u8, platform, "sokol")) {
-        platform_renderer = .sokol;
-    } else if (std.mem.eql(u8, platform, "sdl_soft")) {
-        platform_renderer = .sdl_soft;
+    if (options.platform_renderer == .sdl or options.platform_renderer == .sdl_soft) {
+        const sdl_sdk = sdl.init(b, .{});
+        sdl_sdk.link(exe, .dynamic, sdl.Library.SDL2);
     }
-    const sdl_sdk = sdl.init(b, .{});
 
-    _ = getZimpactModule(b, .{
-        .optimize = optimize,
-        .target = target,
-        .platform_renderer = platform_renderer,
-    });
+    const install_exe = b.addInstallArtifact(exe, .{});
+    install_exe.step.dependOn(options.assets_step);
+    b.getInstallStep().dependOn(&install_exe.step);
 
-    // build lib
-    const lib = b.addStaticLibrary(.{
-        .name = "zimpact",
-        .root_source_file = b.path("src/zimpact/zimpact.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(lib);
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step(b.fmt("run", .{}), b.fmt("Run {s}.zig example", .{options.name}));
+    run_cmd.step.dependOn(&install_exe.step);
+    run_step.dependOn(&run_cmd.step);
 
-    // build docs
-    const docs = b.addInstallDirectory(.{
-        .source_dir = lib.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-    });
-    var docs_step = b.step("docs", "Build docs");
-    docs_step.dependOn(&docs.step);
-    b.getInstallStep().dependOn(docs_step);
-
-    const asset_dir = "samples/zdrop/assets";
-    const assets_step = try buildAssets(b, asset_dir);
-
-    // build Z Drop sample
-    const sample: []const u8 = "zdrop";
-    if (!target.result.cpu.arch.isWasm()) {
-        // for native platforms, build into a regular executable
-        const exe = b.addExecutable(.{
-            .name = sample,
-            .root_source_file = b.path(b.fmt("samples/{s}/main.zig", .{sample})),
-            .target = target,
-            .optimize = optimize,
-        });
-        if (platform_renderer == .sdl or platform_renderer == .sdl_soft) {
-            sdl_sdk.link(exe, .dynamic, sdl.Library.SDL2);
-        }
-        exe.root_module.addImport("zimpact", mod_zi);
-        const install_exe = b.addInstallArtifact(exe, .{});
-        install_exe.step.dependOn(assets_step);
-        b.getInstallStep().dependOn(&install_exe.step);
-
-        const run_cmd = b.addRunArtifact(exe);
-        const run_step = b.step(b.fmt("run", .{}), b.fmt("Run {s}.zig example", .{sample}));
-        run_cmd.step.dependOn(&install_exe.step);
-        run_step.dependOn(&run_cmd.step);
-
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-    } else {
-        try buildWeb(b, .{
-            .root_source_file = b.path("samples/zdrop/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .assets_step = assets_step,
-            .mod_zi = mod_zi,
-            .output_name = "zdrop",
-        });
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
 }
 
@@ -178,55 +200,50 @@ fn convert(b: *std.Build, tool: *std.Build.Step.Compile, input: []const u8, outp
     const tool_step = b.addRunArtifact(tool);
     tool_step.addFileArg(b.path(input));
     const out = tool_step.addOutputFileArg(std.fs.path.basename(output));
-    // b.getInstallStep().dependOn(&b.addInstallBinFile(out, output).step);
     return b.addInstallBinFile(out, output);
 }
 
-pub const BuildWebOptions = struct {
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-    root_source_file: ?std.Build.LazyPath = null,
+const BuildWasmOptions = struct {
+    mod_main: *std.Build.Module,
+    dep_sokol: *std.Build.Dependency,
     assets_step: *std.Build.Step,
-    mod_zi: *std.Build.Module,
-    output_name: []const u8,
+    shell_file_path: std.Build.LazyPath,
 };
 
 // for web builds, the Zig code needs to be built into a library and linked with the Emscripten linker
-pub fn buildWeb(b: *std.Build, options: BuildWebOptions) !void {
-    const dep_sokol = b.dependency("sokol", .{
-        .target = options.target,
-        .optimize = options.optimize,
+pub fn buildWasm(b: *std.Build, opts: BuildWasmOptions) !void {
+    // build the main file into a library, this is because the WASM 'exe'
+    // needs to be linked in a separate build step with the Emscripten linker
+    const demo = b.addLibrary(.{
+        .name = "demo",
+        .root_module = opts.mod_main,
     });
-    const sample = b.addStaticLibrary(.{
-        .name = options.output_name,
-        .target = options.target,
-        .optimize = options.optimize,
-        .root_source_file = options.root_source_file,
-    });
-    sample.root_module.addImport("zimpact", options.mod_zi);
-    sample.root_module.addImport("sokol", dep_sokol.module("sokol"));
+
+    // get the Emscripten SDK dependency from the sokol dependency
+    const dep_emsdk = opts.dep_sokol.builder.dependency("emsdk", .{});
 
     // create a build step which invokes the Emscripten linker
-    const emsdk = dep_sokol.builder.dependency("emsdk", .{});
     const link_step = try sokol.emLinkStep(b, .{
-        .lib_main = sample,
-        .target = options.target,
-        .optimize = options.optimize,
-        .emsdk = emsdk,
+        .lib_main = demo,
+        .target = opts.mod_main.resolved_target.?,
+        .optimize = opts.mod_main.optimize.?,
+        .emsdk = dep_emsdk,
         .use_webgl2 = true,
         .use_emmalloc = true,
         .use_filesystem = true,
-        .shell_file_path = b.path(sdkPath("/web/shell.html")),
+        .shell_file_path = opts.shell_file_path,
         .extra_args = &.{ "-sUSE_OFFSET_CONVERTER=1", "--preload-file", "zig-out/bin/assets@assets" },
     });
+    // attach to default target
+    b.getInstallStep().dependOn(&link_step.step);
     // ...and a special run step to start the web build output via 'emrun'
-    const run = sokol.emRunStep(b, .{ .name = options.output_name, .emsdk = emsdk });
-    run.step.dependOn(options.assets_step);
+    const run = sokol.emRunStep(b, .{ .name = "demo", .emsdk = dep_emsdk });
+    run.step.dependOn(opts.assets_step);
     run.step.dependOn(&link_step.step);
-    b.step("run", "Run zdrop").dependOn(&run.step);
+    b.step("run", "Run demo").dependOn(&run.step);
 }
 
-pub fn buildAssets(b: *std.Build, asset_dir: []const u8) !*std.Build.Step {
+fn addQoiConverter(b: *std.Build) *std.Build.Step.Compile {
     // build qoiconv executable
     const qoiconv_exe = b.addExecutable(.{
         .name = "qoiconv",
@@ -235,13 +252,14 @@ pub fn buildAssets(b: *std.Build, asset_dir: []const u8) !*std.Build.Step {
     });
     qoiconv_exe.linkLibC();
     qoiconv_exe.addCSourceFile(.{
-        .file = .{ .cwd_relative = sdkPath("/tools/qoiconv.c") },
+        .file = b.path("tools/qoiconv.c"),
         .flags = &[_][]const u8{"-std=c99"},
     });
 
-    const qoiconv_step = b.step("qoiconv", "Build qoiconv");
-    qoiconv_step.dependOn(&qoiconv_exe.step);
+    return qoiconv_exe;
+}
 
+fn addQoaConverter(b: *std.Build) *std.Build.Step.Compile {
     // build qoaconv executable
     const qoaconv_exe = b.addExecutable(.{
         .name = "qoaconv",
@@ -250,19 +268,23 @@ pub fn buildAssets(b: *std.Build, asset_dir: []const u8) !*std.Build.Step {
     });
     qoaconv_exe.linkLibC();
     qoaconv_exe.addCSourceFile(.{
-        .file = .{ .cwd_relative = sdkPath("/tools/qoaconv.c") },
+        .file = b.path("tools/qoaconv.c"),
         .flags = &[_][]const u8{"-std=c99"},
     });
 
-    const qoaconv_step = b.step("qoaconv", "Build qoaconv");
-    qoaconv_step.dependOn(&qoaconv_exe.step);
+    return qoaconv_exe;
+}
 
+const BuildAssetsOptions = struct {
+    assets_step: *std.Build.Step,
+    asset_dir: []const u8,
+    qoiconv_exe: *std.Build.Step.Compile,
+    qoaconv_exe: *std.Build.Step.Compile,
+};
+
+pub fn buildAssets(b: *std.Build, options: BuildAssetsOptions) !void {
     // convert the assets and install them
-    const assets_step = b.step("assets", "Build assets");
-    assets_step.dependOn(qoiconv_step);
-    assets_step.dependOn(qoaconv_step);
-
-    if (std.fs.cwd().openDir(asset_dir, .{ .iterate = true })) |dir| {
+    if (std.fs.cwd().openDir(options.asset_dir, .{ .iterate = true })) |dir| {
         var walker = try dir.walk(b.allocator);
         defer walker.deinit();
 
@@ -273,26 +295,24 @@ pub fn buildAssets(b: *std.Build, asset_dir: []const u8) !*std.Build.Step {
                     const ext = std.fs.path.extension(assets_file.path);
                     const file = std.fs.path.stem(assets_file.basename);
 
-                    const input = b.fmt("{s}/{s}", .{ asset_dir, assets_file.path });
+                    const input = b.fmt("{s}/{s}", .{ options.asset_dir, assets_file.path });
                     const out_dir = std.fs.path.dirname(assets_file.path);
                     if (std.mem.eql(u8, ext, ".png")) {
                         // convert .png to .qoi
                         const output = if (out_dir) |d| b.fmt("assets/{s}/{s}.qoi", .{ d, file }) else b.fmt("assets/{s}.qoi", .{file});
-                        assets_step.dependOn(&convert(b, qoiconv_exe, input, output).step);
+                        options.assets_step.dependOn(&convert(b, options.qoiconv_exe, input, output).step);
                     } else if (std.mem.eql(u8, ext, ".wav")) {
                         // convert .wav to .qoa
                         const output = if (out_dir) |d| b.fmt("assets/{s}/{s}.qoa", .{ d, file }) else b.fmt("assets/{s}.qoa", .{file});
-                        assets_step.dependOn(&convert(b, qoaconv_exe, input, output).step);
+                        options.assets_step.dependOn(&convert(b, options.qoaconv_exe, input, output).step);
                     } else {
                         // just copy the asset
                         const output = if (out_dir) |d| b.fmt("assets/{s}/{s}{s}", .{ d, file, ext }) else b.fmt("assets/{s}{s}", .{ file, ext });
-                        assets_step.dependOn(&b.addInstallFileWithDir(b.path(input), .bin, output).step);
+                        options.assets_step.dependOn(&b.addInstallFileWithDir(b.path(input), .bin, output).step);
                     }
                 },
                 else => {},
             }
         }
     } else |_| {}
-
-    return assets_step;
 }
